dbbf7c40fade15631d3cf0591654a665c2c9540b
Bradley Schofield <ionicisere@gmail.com>
2023-09-06 10:48:16 +0100

    Fix Storage and start adding LFS
    also added a new .env configure tool

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 const { Client } = require('node-appwrite')
 const { handleAuth } = require('./services/auth')
 const { handleDatabases } = require('./services/databases')
@@ -7,11 +8,46 @@ const { handleFunctions } = require('./services/functions')
 require('dotenv').config()
 
 async function main () {
+  let questions = [
+    {
+      type: 'input',
+      name: 'appwriteEndpoint',
+      message: 'What is your Appwrite endpoint?',
+      default: process.env.APPWRITE_ENDPOINT ?? 'http://localhost/v1'
+    },
+    {
+      type: 'input',
+      name: 'appwriteKey',
+      message: 'What is your Appwrite API Key?'
+    },
+    {
+      type: 'input',
+      name: 'appwriteProjectID',
+      message: 'What is your Appwrite Project ID?'
+    }
+  ];
+  
+  if (process.env.APPWRITE_ENDPOINT) {
+    questions[0].default = process.env.APPWRITE_ENDPOINT
+  }
+
+  if (process.env.APPWRITE_API_KEY) {
+    questions[1].default = process.env.APPWRITE_API_KEY
+  }
+
+  if (process.env.APPWRITE_PROJECT_ID) {
+    questions[2].default = process.env.APPWRITE_PROJECT_ID
+  }
+
+  const { appwriteEndpoint, appwriteKey, appwriteProjectID } = await inquirer.prompt(questions);
+
+  fs.writeFileSync('./.env', `APPWRITE_ENDPOINT=${appwriteEndpoint}\nAPPWRITE_API_KEY=${appwriteKey}\nAPPWRITE_PROJECT_ID=${appwriteProjectID}`)
+
   const appwrite = new Client()
   appwrite
-    .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setKey(process.env.APPWRITE_API_KEY)
-    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setEndpoint(appwriteEndpoint)
+    .setKey(appwriteKey)
+    .setProject(appwriteProjectID)
 
   const { services } = await inquirer.prompt([
     {
--- src/services/storage.js
+++ src/services/storage.js
@@ -1,48 +1,100 @@
-const { Storage, InputFile } = require('node-appwrite')
-const { faker } = require('@faker-js/faker')
-const inquirer = require('inquirer')
-const ProgressBar = require('progress')
+const { Storage, InputFile } = require("node-appwrite");
+const { faker } = require("@faker-js/faker");
+const inquirer = require("inquirer");
+const ProgressBar = require("progress");
 
-async function handleStorage (appwrite) {
-  const buckets = await generateBuckets(appwrite)
+async function handleStorage(appwrite) {
+  const buckets = await generateBuckets(appwrite);
 
   if (!buckets.length) {
-    return
+    return;
   }
 
-  await generateFiles(appwrite, buckets)
+  const { useLFS } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "useLFS",
+      message:
+        "Would you like to use Large Files? (This will take significantly longer)",
+    },
+  ]);
+
+  if (useLFS) {
+    const { maxLFSFileSize } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "maxLFSFileSize",
+        message: "What is the maximum file size you would like to generate?",
+        choices: [
+          {
+            name: "1MB",
+            value: 1 * 1024 * 1024,
+          },
+          {
+            name: "5MB",
+            value: 5 * 1024 * 1024,
+          },
+          {
+            name: "10MB",
+            value: 10 * 1024 * 1024,
+          },
+          {
+            name: "50MB",
+            value: 50 * 1024 * 1024,
+          },
+          {
+            name: "100MB",
+            value: 100 * 1024 * 1024,
+          },
+          {
+            name: "500MB",
+            value: 500 * 1024 * 1024,
+          },
+          {
+            name: "1GB",
+            value: 1024 * 1024 * 1024,
+          },
+        ],
+      },
+    ]);
+  }
+
+  await generateFiles(appwrite, buckets, useLFS);
 }
 
-async function generateBuckets (appwrite) {
-  const storageClient = new Storage(appwrite)
+async function generateBuckets(appwrite) {
+  const storageClient = new Storage(appwrite);
 
   const { bucketsNo } = await inquirer.prompt([
     {
-      type: 'number',
-      name: 'bucketsNo',
-      message: 'How many buckets would you like to generate?'
+      type: "number",
+      name: "bucketsNo",
+      message: "How many buckets would you like to generate?",
+    },
+  ]);
+
+  const buckets = [];
+
+  const bar = new ProgressBar(
+    "Creating new buckets... [:bar] :current/:total",
+    {
+      total: bucketsNo,
     }
-  ])
-
-  const buckets = []
-
-  const bar = new ProgressBar('Creating new buckets... [:bar] :current/:total', {
-    total: bucketsNo
-  })
+  );
 
   for (let i = 0; i < bucketsNo; i += 1) {
     await storageClient
       .createBucket(
-        'unique()',
+        "unique()",
         `${faker.word.adjective()} ${faker.word.noun()}`
       )
       .then((response) => {
-        buckets.push(response)
-        bar.tick()
-      })
+        buckets.push(response);
+        bar.tick();
+      });
   }
 
-  return buckets
+  return buckets;
 }
 
 /**
@@ -55,7 +107,7 @@ async function generateBuckets (appwrite) {
  * @param {*} totalChunks
  * @returns
  */
-async function uploadChunkToAppwrite (
+async function uploadChunkToAppwrite(
   chunk,
   storage,
   bucketId,
@@ -63,107 +115,120 @@ async function uploadChunkToAppwrite (
   chunkIndex,
   totalChunks
 ) {
-  const blob = new Blob([chunk])
+  const blob = new Blob([chunk]);
   const inputFile = await InputFile.fromBlob(
     blob,
     `${faker.person.firstName()}.jpg`
-  )
+  );
 
-  const headers = chunkIndex === 0 ? {} : { 'x-appwrite-id': fileId }
-  return storage.createFile(bucketId, fileId, inputFile, undefined)
+  return storage.createFile(bucketId, fileId, inputFile);
 }
 
-async function fetchChunk (url, startByte, endByte) {
+async function fetchChunk(url, startByte, endByte) {
   const headers = new Headers({
-    Range: `bytes=${startByte}-${endByte}`
-  })
+    Range: `bytes=${startByte}-${endByte}`,
+  });
 
-  const response = await fetch(url, { headers })
-  return response.arrayBuffer()
+  const response = await fetch(url, { headers });
+
+  return response.arrayBuffer();
 }
 
-async function fetchTotalSize (url) {
-  const response = await fetch(url, { method: 'HEAD' })
-  return parseInt(response.headers.get('Content-Length'), 10)
+async function fetchTotalSize(url) {
+  const response = await fetch(url, { method: "HEAD" });
+  return parseInt(response.headers.get("Content-Length"), 10);
 }
 
-async function streamUploadFromURL (url, storageClient, bucketId) {
-  const CHUNK_SIZE = 5 * 1024 * 1024 // 5MB
-  const totalSize = await fetchTotalSize(url)
+async function streamUploadFromURL(url, storageClient, bucketId) {
+  const CHUNK_SIZE = 5 * 1024 * 1024; // 5MB
+  const totalSize = await fetchTotalSize(url);
 
-  let fileId = null
+  let fileId = null;
 
-  for (let i = 0; i < totalSize; i += CHUNK_SIZE) {
-    const endByte = Math.min(i + CHUNK_SIZE - 1, totalSize - 1)
+  for (
+    let currentChunk = 0;
+    currentChunk < totalSize;
+    currentChunk += CHUNK_SIZE
+  ) {
+    const endByte = Math.min(currentChunk + CHUNK_SIZE - 1, totalSize - 1);
 
-    const chunk = await fetchChunk(url, i, endByte)
+    const chunk = await fetchChunk(url, currentChunk, endByte);
     const response = await uploadChunkToAppwrite(
       chunk,
       storageClient,
       bucketId,
-      fileId || '',
-      Math.floor(i / CHUNK_SIZE),
+      fileId || "unique()",
+      Math.floor(currentChunk / CHUNK_SIZE),
       Math.ceil(totalSize / CHUNK_SIZE)
-    )
+    );
 
     if (!fileId) {
-      fileId = response.$id
+      fileId = response.$id;
     }
   }
 
-  return fileId
+  return fileId;
 }
 
-async function generateFiles (appwrite, buckets) {
-  const storageClient = new Storage(appwrite)
+async function generateFiles(appwrite, buckets, useLFS) {
+  const storageClient = new Storage(appwrite);
 
   const { filesNo } = await inquirer.prompt([
     {
-      type: 'number',
-      name: 'filesNo',
-      message: 'How many files would you like to generate per bucket?'
-    }
-  ])
+      type: "number",
+      name: "filesNo",
+      message: "How many files would you like to generate per bucket?",
+    },
+  ]);
+
+  if (useLFS) {
+    console.log('LFs is not yet supported, falling back to regular files');
+  }
 
   const bar = new ProgressBar(
-    'Uploading files (This may take a while) ... [:bar] :current/:total',
+    "Uploading files (This may take a while) ... [:bar] :current/:total",
     {
-      total: buckets.length * filesNo
+      total: buckets.length * filesNo,
     }
-  )
+  );
 
   for (let i = 0; i < buckets.length; i += 1) {
-    const bucket = buckets[i]
+    const bucket = buckets[i];
 
     for (let r = 0; r < filesNo; r += 1) {
-      const fileURL = getFakeFileURL()
+      const fileURL = useLFS ? getFakeLFSFileURL() : getFakeFileURL();
 
       await streamUploadFromURL(fileURL, storageClient, bucket.$id).then(
         (response) => {
-          bar.tick()
+          bar.tick();
         }
-      )
+      );
     }
   }
 }
 
-function getFakeFileURL () {
-  let type = faker.helpers.arrayElement([
-    'image',
-    'video',
-    'audio',
-    'text',
-    'binary'
-  ])
+function getFakeLFSFileURL() {
+  //temp
+  return getFakeFileURL();
+}
 
-  type = 'image' // temp
+function getFakeFileURL() {
+  let type = faker.helpers.arrayElement([
+    "image",
+    "video",
+    "audio",
+    "text",
+    "binary",
+  ]);
+
+  type = "image"; // temp
 
   switch (type) {
-    case 'image':
-      return faker.image.urlLoremFlickr({ category: 'cats' })
+    case "image":
+      return faker.image.urlLoremFlickr({ category: "cats" });
   }
 }
 
 module.exports = {
-  handleStorage
-}
+  handleStorage,
+};
